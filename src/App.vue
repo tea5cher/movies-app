a6de2213e22cb7e50085fd84d34275e9b4d00bc9
Egor Shkurov <grafushka10@mail.ru>
2022-08-03 14:29:22 +0300

    test commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      <b-button>Кнопка</b-button>
+      <b-button variant="danger">Кнопка</b-button>
+      <b-button variant="success">Кнопка</b-button>
+      <b-button variant="outline-primary">Кнопка</b-button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+ 
   }
 }
 </script>
